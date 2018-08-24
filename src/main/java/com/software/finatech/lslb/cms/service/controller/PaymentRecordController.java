02e4c03d9785593f38ce7b19776aaf181ef44b35
dayee <adeboludeyi@gmail.com>
2018-08-24 18:40:19 +0100

    added get alll approvers

--- src/main/java/com/software/finatech/lslb/cms/service/controller/PaymentRecordController.java
+++ src/main/java/com/software/finatech/lslb/cms/service/controller/PaymentRecordController.java
@@ -57,4 +57,15 @@ public class PaymentRecordController extends BaseController {
     public Mono<ResponseEntity> getAllPaymentStatus() {
         return paymentRecordService.getAllPaymentStatus();
     }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/all-approvers")
+    @ApiOperation(value = "Create Fee Configuration", response = EnumeratedFactDto.class,responseContainer = "List",consumes = "application/json")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 401, message = "You are not authorized access the resource"),
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 404, message = "Not Found")})
+    public Mono<ResponseEntity> getAllApprovers() {
+        return paymentRecordService.getAllApprovers();
+    }
 }
