24942bde8b9d5394b98c72bbf01d7036e6f5350c
dayee <adeboludeyi@gmail.com>
2018-08-31 18:36:19 +0100

    added agent institution

--- src/main/java/com/software/finatech/lslb/cms/service/domain/Agent.java
+++ src/main/java/com/software/finatech/lslb/cms/service/domain/Agent.java
@@ -3,18 +3,77 @@ package com.software.finatech.lslb.cms.service.domain;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @SuppressWarnings("serial")
 @Document(collection = "Agents")
 public class Agent extends AbstractFact {
 
     protected String passportId;
-    protected String institutionId;
     protected String firstName;
     protected String lastName;
     protected String fullName;
     protected String emailAddress;
     protected String phoneNumber;
-    protected String gameTypeId;
+    protected Set<AgentInstitution> agentInstitutionList = new HashSet<>();
+
+
+    public String getPassportId() {
+        return passportId;
+    }
+
+    public void setPassportId(String passportId) {
+        this.passportId = passportId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Set<AgentInstitution> getAgentInstitutionList() {
+        return agentInstitutionList;
+    }
+
+    public void setAgentInstitutionList(Set<AgentInstitution> agentInstitutionList) {
+        this.agentInstitutionList = agentInstitutionList;
+    }
 
     @Override
     public String getFactName() {
---
+++ src/main/java/com/software/finatech/lslb/cms/service/domain/AgentInstitution.java
@@ -0,0 +1,22 @@
+package com.software.finatech.lslb.cms.service.domain;
+
+public class AgentInstitution {
+    private String institutionId;
+    private String gameTypeId;
+
+    public String getInstitutionId() {
+        return institutionId;
+    }
+
+    public void setInstitutionId(String institutionId) {
+        this.institutionId = institutionId;
+    }
+
+    public String getGameTypeId() {
+        return gameTypeId;
+    }
+
+    public void setGameTypeId(String gameTypeId) {
+        this.gameTypeId = gameTypeId;
+    }
+}
