3fcfcfa8981cc5a4bde3766f94dba6542919782b
Anjorin Samuel <samuel.anjorin@venturegardengroup.com>
2018-09-01 13:27:19 +0100

    updated find payment records

--- src/main/java/com/software/finatech/lslb/cms/service/controller/PaymentRecordController.java
+++ src/main/java/com/software/finatech/lslb/cms/service/controller/PaymentRecordController.java
@@ -1,6 +1,8 @@
 package com.software.finatech.lslb.cms.service.controller;
 
 
+import com.software.finatech.lslb.cms.service.domain.License;
+import com.software.finatech.lslb.cms.service.domain.PaymentRecord;
 import com.software.finatech.lslb.cms.service.dto.EnumeratedFactDto;
 import com.software.finatech.lslb.cms.service.dto.PaymentRecordCreateDto;
 import com.software.finatech.lslb.cms.service.dto.PaymentRecordDto;
@@ -9,13 +11,17 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @Api(value = "Payment Records", description = "For everything related to payment records", tags = "")
 @RestController
@@ -49,6 +55,33 @@ public class PaymentRecordController extends BaseController {
         return paymentRecordService.findAllPaymentRecords(page, pageSize, sortType, sortParam, approverId, institutionId,  feeId, httpServletResponse);
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "/by-institutionId-gameTypeId", params={"institutionId","gameTypeId","mostRecent"})
+    @ApiOperation(value = "Get specific payment Status", response = EnumeratedFactDto.class,responseContainer = "List",consumes = "application/json")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 401, message = "You are not authorized access the resource"),
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 404, message = "Not Found")})
+    public Mono<ResponseEntity> getPaymentRecordsByInstitution(@RequestParam("institutionId") String institutionId,@RequestParam("gameTypeId") String gameTypeId,@RequestParam("mostRecent") String isMostRecent ) {
+        boolean isMostRecentI;
+        if(StringUtils.isEmpty(isMostRecent)){
+            isMostRecentI=false;
+        }else{
+            isMostRecentI= Boolean.valueOf(isMostRecent);
+        }
+        if(StringUtils.isEmpty(institutionId)){
+            return Mono.just(new ResponseEntity<>("Provide InstitutionId", HttpStatus.BAD_REQUEST));
+
+        }
+        List<PaymentRecord> paymentsRecords = paymentRecordService.findPayments(institutionId,gameTypeId,isMostRecentI);
+        List<PaymentRecordDto> paymentRecordDtos= new ArrayList<>();
+        paymentsRecords.stream().forEach(paymentRecord -> {
+            paymentRecordDtos.add(paymentRecord.convertToDto());
+        });
+        return Mono.just(new ResponseEntity<>(paymentRecordDtos, HttpStatus.OK));
+
+    }
+
     @RequestMapping(method = RequestMethod.GET, value = "/all-payment-status")
     @ApiOperation(value = "Get all payment Status", response = EnumeratedFactDto.class,responseContainer = "List",consumes = "application/json")
     @ApiResponses(value = {
@@ -59,6 +92,7 @@ public class PaymentRecordController extends BaseController {
     public Mono<ResponseEntity> getAllPaymentStatus() {
         return paymentRecordService.getAllPaymentStatus();
     }
+
 
 
 
