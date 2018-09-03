1f7f842f6c22fda132302a36f3aecdf358031946
dayee <adeboludeyi@gmail.com>
2018-09-03 09:40:28 +0100

    added renewal fee payment

--- src/main/java/com/software/finatech/lslb/cms/service/referencedata/FeePaymentTypeReferenceData.java
+++ src/main/java/com/software/finatech/lslb/cms/service/referencedata/FeePaymentTypeReferenceData.java
@@ -6,6 +6,7 @@ import com.software.finatech.lslb.cms.service.persistence.MongoRepositoryReactiv
 public class FeePaymentTypeReferenceData {
     public static final String APPLICATION_FEE_TYPE_ID = "01";
     public static final String LICENSE_FEE_TYPE_ID= "02";
+    public static final String LICENSE_RENEWAL_FEE_TYPE_ID= "03";
 
     public static void load(MongoRepositoryReactiveImpl mongoRepositoryReactive) {
         FeePaymentType feePaymentType1 = (FeePaymentType) mongoRepositoryReactive.findById(APPLICATION_FEE_TYPE_ID, FeePaymentType.class).block();
@@ -22,7 +23,15 @@ public class FeePaymentTypeReferenceData {
         }
         feePaymentType2.setName("Licensing Fees");
 
+        FeePaymentType feePaymentType3 = (FeePaymentType) mongoRepositoryReactive.findById(LICENSE_RENEWAL_FEE_TYPE_ID FeePaymentType.class).block();
+        if (feePaymentType3 == null) {
+            feePaymentType3 = new FeePaymentType();
+            feePaymentType3.setId(LICENSE_RENEWAL_FEE_TYPE_ID);
+        }
+        feePaymentType3.setName("Licensing Renewal Fees");
+
         mongoRepositoryReactive.saveOrUpdate(feePaymentType1);
         mongoRepositoryReactive.saveOrUpdate(feePaymentType2);
+        mongoRepositoryReactive.saveOrUpdate(feePaymentType3);
     }
 }
