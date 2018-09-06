8ab22c4a37901509c82035d6590b7ab0fd70b3d2
dayee <adeboludeyi@gmail.com>
2018-09-06 11:49:29 +0100

    null check in payment record dto

--- src/main/java/com/software/finatech/lslb/cms/service/model/vigipay/VigipayCreateCustomer.java
+++ src/main/java/com/software/finatech/lslb/cms/service/model/vigipay/VigipayCreateCustomer.java
@@ -1,4 +1,149 @@
 package com.software.finatech.lslb.cms.service.model.vigipay;
 
 public class VigipayCreateCustomer {
+    private String Name;
+    private String Code;
+    private String website;
+    private String AddressLine1;
+    private String AddressLine2;
+    private String CountryCode;
+    private String PostCode;
+    private String Locale;
+    private String Region;
+    private String CorporateCode;
+    private String CustomerCorporateCode;
+    private String ContactPersonEmail;
+    private String ContactPersonFirstName;
+    private String ContactPersonLastName;
+    private String ContactPersonTitle;
+    private String ContactPersonPhone;
+
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getCode() {
+        return Code;
+    }
+
+    public void setCode(String code) {
+        Code = code;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getAddressLine1() {
+        return AddressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        AddressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return AddressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        AddressLine2 = addressLine2;
+    }
+
+    public String getCountryCode() {
+        return CountryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        CountryCode = countryCode;
+    }
+
+    public String getPostCode() {
+        return PostCode;
+    }
+
+    public void setPostCode(String postCode) {
+        PostCode = postCode;
+    }
+
+    public String getLocale() {
+        return Locale;
+    }
+
+    public void setLocale(String locale) {
+        Locale = locale;
+    }
+
+    public String getRegion() {
+        return Region;
+    }
+
+    public void setRegion(String region) {
+        Region = region;
+    }
+
+    public String getCorporateCode() {
+        return CorporateCode;
+    }
+
+    public void setCorporateCode(String corporateCode) {
+        CorporateCode = corporateCode;
+    }
+
+    public String getCustomerCorporateCode() {
+        return CustomerCorporateCode;
+    }
+
+    public void setCustomerCorporateCode(String customerCorporateCode) {
+        CustomerCorporateCode = customerCorporateCode;
+    }
+
+    public String getContactPersonEmail() {
+        return ContactPersonEmail;
+    }
+
+    public void setContactPersonEmail(String contactPersonEmail) {
+        ContactPersonEmail = contactPersonEmail;
+    }
+
+    public String getContactPersonFirstName() {
+        return ContactPersonFirstName;
+    }
+
+    public void setContactPersonFirstName(String contactPersonFirstName) {
+        ContactPersonFirstName = contactPersonFirstName;
+    }
+
+    public String getContactPersonLastName() {
+        return ContactPersonLastName;
+    }
+
+    public void setContactPersonLastName(String contactPersonLastName) {
+        ContactPersonLastName = contactPersonLastName;
+    }
+
+    public String getContactPersonTitle() {
+        return ContactPersonTitle;
+    }
+
+    public void setContactPersonTitle(String contactPersonTitle) {
+        ContactPersonTitle = contactPersonTitle;
+    }
+
+    public String getContactPersonPhone() {
+        return ContactPersonPhone;
+    }
+
+    public void setContactPersonPhone(String contactPersonPhone) {
+        ContactPersonPhone = contactPersonPhone;
+    }
 }
