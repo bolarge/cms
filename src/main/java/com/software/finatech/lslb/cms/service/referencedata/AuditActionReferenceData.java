d3f763701ebc577fd93fe0022d2d0fad741ad833
dayee <adeboludeyi@gmail.com>
2018-10-09 14:48:51 +0100

    added audit actions

--- src/main/java/com/software/finatech/lslb/cms/service/referencedata/AuditActionReferenceData.java
+++ src/main/java/com/software/finatech/lslb/cms/service/referencedata/AuditActionReferenceData.java
@@ -6,10 +6,21 @@ import com.software.finatech.lslb.cms.service.persistence.MongoRepositoryReactiv
 
 public class AuditActionReferenceData {
     public static final String LOGIN_ID = "1";
+    public static final String USER_ID = "2";
+    public static final String APPLICATION_ID = "4";
+    public static final String FEE_ID = "5";
+    public static final String SCHEDULED_MEETING_ID = "6";
+    public static final String LICENCE_ID = "7";
+    public static final String CASE_ID = "8";
+    public static final String PAYMENT_ID = "9";
+    public static final String CUSTOMER_COMPLAIN = "10";
+    public static final String GAMING_MACHINE_ID = "11";
+    public static final String AGENT_ID = "12";
 
-    public static void load(MongoRepositoryReactiveImpl mongoRepositoryReactive){
-        AuditAction action = (AuditAction) mongoRepositoryReactive.findById(LOGIN_ID,AuditAction.class).block();
-        if(action==null){
+
+    public static void load(MongoRepositoryReactiveImpl mongoRepositoryReactive) {
+        AuditAction action = (AuditAction) mongoRepositoryReactive.findById(LOGIN_ID, AuditAction.class).block();
+        if (action == null) {
             action = new AuditAction();
             action.setId(LOGIN_ID);
         }
@@ -17,17 +28,17 @@ public class AuditActionReferenceData {
         action.setName("Login");
         mongoRepositoryReactive.saveOrUpdate(action);
 
-        action = (AuditAction) mongoRepositoryReactive.findById("2",AuditAction.class).block();
-        if(action==null){
+        action = (AuditAction) mongoRepositoryReactive.findById(USER_ID, AuditAction.class).block();
+        if (action == null) {
             action = new AuditAction();
-            action.setId("2");
+            action.setId(USER_ID);
         }
         action.setDescription("User");
         action.setName("User");
         mongoRepositoryReactive.saveOrUpdate(action);
 
-        action = (AuditAction) mongoRepositoryReactive.findById("3",AuditAction.class).block();
-        if(action==null){
+        action = (AuditAction) mongoRepositoryReactive.findById("3", AuditAction.class).block();
+        if (action == null) {
             action = new AuditAction();
             action.setId("3");
         }
@@ -35,5 +46,85 @@ public class AuditActionReferenceData {
         action.setName("File Upload");
         mongoRepositoryReactive.saveOrUpdate(action);
 
+        action = (AuditAction) mongoRepositoryReactive.findById(APPLICATION_ID, AuditAction.class).block();
+        if (action == null) {
+            action = new AuditAction();
+            action.setId(APPLICATION_ID);
+        }
+        action.setDescription("Application for licence");
+        action.setName("Application");
+        mongoRepositoryReactive.saveOrUpdate(action);
+
+        action = (AuditAction) mongoRepositoryReactive.findById(FEE_ID, AuditAction.class).block();
+        if (action == null) {
+            action = new AuditAction();
+            action.setId(APPLICATION_ID);
+        }
+        action.setDescription("Fee Configurations");
+        action.setName("Fee");
+        mongoRepositoryReactive.saveOrUpdate(action);
+
+        action = (AuditAction) mongoRepositoryReactive.findById(SCHEDULED_MEETING_ID, AuditAction.class).block();
+        if (action == null) {
+            action = new AuditAction();
+            action.setId(SCHEDULED_MEETING_ID);
+        }
+        action.setDescription("Scheduled meetings");
+        action.setName("Scheduled Meeting");
+        mongoRepositoryReactive.saveOrUpdate(action);
+
+        action = (AuditAction) mongoRepositoryReactive.findById(LICENCE_ID, AuditAction.class).block();
+        if (action == null) {
+            action = new AuditAction();
+            action.setId(LICENCE_ID);
+        }
+        action.setDescription("Licence");
+        action.setName("Licence");
+        mongoRepositoryReactive.saveOrUpdate(action);
+
+        action = (AuditAction) mongoRepositoryReactive.findById(CASE_ID, AuditAction.class).block();
+        if (action == null) {
+            action = new AuditAction();
+            action.setId(CASE_ID);
+        }
+        action.setDescription("Logged Case");
+        action.setName("Case");
+        mongoRepositoryReactive.saveOrUpdate(action);
+
+        action = (AuditAction) mongoRepositoryReactive.findById(PAYMENT_ID, AuditAction.class).block();
+        if (action == null) {
+            action = new AuditAction();
+            action.setId(PAYMENT_ID);
+        }
+        action.setDescription("Payments");
+        action.setName("Payment");
+        mongoRepositoryReactive.saveOrUpdate(action);
+
+        action = (AuditAction) mongoRepositoryReactive.findById(CUSTOMER_COMPLAIN, AuditAction.class).block();
+        if (action == null) {
+            action = new AuditAction();
+            action.setId(CUSTOMER_COMPLAIN);
+        }
+        action.setDescription("Customer Complaints");
+        action.setName("Customer Complain");
+        mongoRepositoryReactive.saveOrUpdate(action);
+
+        action = (AuditAction) mongoRepositoryReactive.findById(GAMING_MACHINE_ID, AuditAction.class).block();
+        if (action == null) {
+            action = new AuditAction();
+            action.setId(GAMING_MACHINE_ID);
+        }
+        action.setDescription("Gaming Machine");
+        action.setName("Gaming Machine");
+        mongoRepositoryReactive.saveOrUpdate(action);
+
+        action = (AuditAction) mongoRepositoryReactive.findById(AGENT_ID, AuditAction.class).block();
+        if (action == null) {
+            action = new AuditAction();
+            action.setId(AGENT_ID);
+        }
+        action.setDescription("AGENT");
+        action.setName("AGENT");
+        mongoRepositoryReactive.saveOrUpdate(action);
     }
 }
