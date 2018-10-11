8c177c4c74f89b001c8dbd75f0da0f1f2ab759f8
dayee <adeboludeyi@gmail.com>
2018-10-11 12:33:27 +0100

    changed agent minimum phone umber

--- src/main/java/com/software/finatech/lslb/cms/service/dto/AgentUpdateDto.java
+++ src/main/java/com/software/finatech/lslb/cms/service/dto/AgentUpdateDto.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.Size;
 public class AgentUpdateDto {
     @NotEmpty(message = "please provide agent id")
     private String id;
-    @Size(min = 9, message = "Phone number should not be less than 7 characters")
+    @Size(min = 9, message = "Phone number should not be less than 9 characters")
     @NotEmpty(message = "please provide agent phone number")
     private String phoneNumber;
     @NotEmpty(message = "please provide agent residential address")
