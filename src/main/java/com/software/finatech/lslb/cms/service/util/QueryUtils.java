d61caeca67550fe8dc5e3e0884bd367fc1346227
adeyiwunmii <adeyi.adebolu@venturegardengroup.com>
2018-12-08 11:29:37 +0100

    changed default start and end dates

--- src/main/java/com/software/finatech/lslb/cms/service/util/QueryUtils.java
+++ src/main/java/com/software/finatech/lslb/cms/service/util/QueryUtils.java
@@ -8,14 +8,22 @@ import org.springframework.data.mongodb.core.query.Query;
 public class QueryUtils {
 
     public static void addDateToQuery(Query query, String startDate, String endDate, String dateProperty) {
-        if (!StringUtils.isEmpty(startDate) && !StringUtils.isEmpty(endDate)) {
-            if (StringUtils.isEmpty(dateProperty)) {
-                dateProperty = "creationDate";
-            }
-
-            LocalDate startDateTime = new LocalDate(startDate);
-            LocalDate endDateTime = new LocalDate(endDate);
-            query.addCriteria(Criteria.where(dateProperty).gte(startDateTime).lte(endDateTime));
+        if (StringUtils.isEmpty(dateProperty)) {
+            dateProperty = "creationDate";
         }
+        LocalDate today = LocalDate.now();
+        LocalDate startDateTime;
+        if (StringUtils.isEmpty(startDate)) {
+            startDateTime = today;
+        } else {
+            startDateTime = new LocalDate(startDate);
+        }
+        LocalDate endDateTime;
+        if (StringUtils.isEmpty(endDate)) {
+            endDateTime = today;
+        } else {
+            endDateTime = new LocalDate(endDate);
+        }
+        query.addCriteria(Criteria.where(dateProperty).gte(startDateTime).lte(endDateTime));
     }
 }
