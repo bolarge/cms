fac79ba493102aca39078119377f20b47393d04c
adeyiwunmii <adeyi.adebolu@venturegardengroup.com>
2018-12-18 16:49:24 +0100

    updated gitignore

---
+++ src/main/java/com/software/finatech/lslb/cms/service/util/httpclient/MyFileManager.java
@@ -0,0 +1,56 @@
+package com.software.finatech.lslb.cms.service.util.httpclient;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+
+@Component
+public class MyFileManager {
+    private static final Logger logger = LoggerFactory.getLogger(MyFileManager.class);
+
+    /**
+     * u see the file name , you should change it to the name of the file
+     * that you want to save most likely the name of the biller
+     * (e.g)let them pass  the name of the biller into the function from the ui
+     */
+    public void writeImageToFile(MultipartFile multipartFile) {
+        File file = new File("smile-networks.jpg");
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(multipartFile.getBytes());
+            fileOutputStream.close();
+
+        } catch (IOException e) {
+            logger.error("IO error occurred while  saving file", e);
+        } catch (Exception e) {
+            logger.error("An error occurred while saving file", e);
+        }
+    }
+
+    public String readImage(String fileName) {
+        File file = new File(fileName);
+        try {
+            if (file.exists() && file.isFile()) {
+                try {
+                    byte[] fileContent = FileUtils.readFileToByteArray(new File(fileName));
+                    return Base64.getEncoder().encodeToString(fileContent);
+                } catch (IOException e) {
+                    logger.error("An io error occurred while reading file", e);
+                    return null;
+                }
+            }
+            return null;
+        } catch (Exception e) {
+            logger.error("An error occurred while reading file", e);
+            return null;
+        }
+    }
+}
