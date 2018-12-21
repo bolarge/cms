d8879bef8ea55db965627d703e45c5c49856c444
adeyiwunmii <adeyi.adebolu@venturegardengroup.com>
2018-12-21 10:17:18 +0100

    changed email

--- src/main/java/com/software/finatech/lslb/cms/service/background_jobs/EmailRetryJob.java
+++ src/main/java/com/software/finatech/lslb/cms/service/background_jobs/EmailRetryJob.java
@@ -4,7 +4,11 @@ import com.software.finatech.lslb.cms.service.domain.FailedEmailNotification;
 import com.software.finatech.lslb.cms.service.persistence.MongoRepositoryReactiveImpl;
 import com.software.finatech.lslb.cms.service.service.EmailService;
 import net.javacrumbs.shedlock.core.SchedulerLock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -15,6 +19,8 @@ import java.util.stream.Collectors;
 
 @Component
 public class EmailRetryJob {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmailRetryJob.class);
     @Autowired
     private MongoRepositoryReactiveImpl mongoRepositoryReactive;
     @Autowired
@@ -22,12 +28,16 @@ public class EmailRetryJob {
     private static final int FIFTEEN_MIN = 15 * 60 * 1000;
     private static final int THREE_MIN = 3 * 60 * 1000;
 
-    @Scheduled(fixedRate = 3 * 60 * 1000, initialDelay = 6000)
-    @SchedulerLock(name = "Send Pending Emails", lockAtMostFor = THREE_MIN, lockAtLeastFor = THREE_MIN)
+    @Scheduled(fixedRate = 60 * 1000, initialDelay = 6000)
+    @SchedulerLock(name = "Send Pending Emails", lockAtMostFor = 60 * 1000, lockAtLeastFor = 60 * 1000)
     public void sendPendingEmails() {
-        ArrayList<FailedEmailNotification> failedEmailNotifications = getPendingFailedEmails();
-        for (FailedEmailNotification failedEmailNotification : failedEmailNotifications) {
-            emailService.sendFailedEmail(failedEmailNotification);
+        try {
+            ArrayList<FailedEmailNotification> failedEmailNotifications = getPendingFailedEmails();
+            for (FailedEmailNotification failedEmailNotification : failedEmailNotifications) {
+                emailService.sendFailedEmail(failedEmailNotification);
+            }
+        } catch (Throwable e) {
+            logger.info("An error occurred while sending pending emails", e);
         }
     }
 
@@ -35,6 +45,9 @@ public class EmailRetryJob {
         Query query = new Query();
         query.addCriteria(Criteria.where("sent").is(false));
         query.addCriteria(Criteria.where("processing").is(false));
+        Sort sort = new Sort(Sort.Direction.DESC, "createdAt");
+        query.with(PageRequest.of(0, 100, sort));
+        query.with(sort);
         return (ArrayList<FailedEmailNotification>) mongoRepositoryReactive.findAll(query, FailedEmailNotification.class).toStream().collect(Collectors.toList());
     }
 }
